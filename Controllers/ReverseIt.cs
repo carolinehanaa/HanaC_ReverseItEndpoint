adbb5cd056a2fcf7624894360317532ae7c29fdd
carolinehanaa <carolinehana28@gmail.com>
2022-10-27 20:49:17 -0700

    first

--- Controllers/ReverseIt.cs
+++ Controllers/ReverseIt.cs
@@ -1,7 +1,7 @@
 /*Caroline Hana
 10-27-2022
 Reverse it - enter random numbers or letters and it will put them in reverse
-Peer Review- Kenneth Fujimura -- Everything works well! The program takes in and accurately reverses whatever string is inputted into it. I couldn't really find any bugs. I really liked how you stored everything in an array that had a 'char' data type. That wasn't something I thought to do but probably should have. Great job Caroline! 10/10 A+
+Peer Review - Kenneth Fujimura -- Everything works well! The program takes in and accurately reverses whatever string is inputted into it. I couldn't really find any bugs. I really liked how you stored everything in an array that had a 'char' data type. That wasn't something I thought to do but probably should have. Great job Caroline! 10/10 A+
 */
 
 using System;
